2d89059dbe8fd2a929e5cfabe8f23d283773b8a5
Carlos <carloseam94@gmail.com>
2022-03-21 11:14:20 -0400

    added: contracts

---
+++ contracts/pickcard.sol
@@ -0,0 +1,42 @@
+ 
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+interface IVerifier {
+    function verifyProof(
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[2] memory input
+    ) external view returns (bool);
+}
+
+contract PickCard {
+    address public verifierAddr;
+
+    constructor(address _verifierAddr) {
+        verifierAddr = _verifierAddr;
+    }
+
+    function verifyProof(
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[2] memory input
+    ) public view returns (bool) {
+        return IVerifier(verifierAddr).verifyProof(a, b, c, input);
+    }
+
+    function verifyPickCard(
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[2] memory input
+    ) public view returns (bool) {
+        require(
+            verifyProof(a, b, c, input),
+            "Filed proof check"
+        );
+        return true;
+    }
+}
